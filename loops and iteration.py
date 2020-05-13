4865c71122a38c8052f0cf4512d9279a354d5771
haasa-swara <swarahaasa@gmail.com>
2020-05-13 09:25:33 -0400

    uploading

---
+++ loops and iteration.py
@@ -0,0 +1,25 @@
+#*****Loops and iteration*****
+###Assignment-5.2###
+#Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. Once 'done' is entered,
+# print out the largest and smallest of the numbers. If the user enters anything other than a valid number
+# catch it with a try/except and put out an appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below.
+#Desired output -Invalid input
+# Maximum is 10
+# Minimum is 2
+largest=0
+smallest=float('inf')
+while True:
+    number=input("Enter a number")
+    if number=='done':
+        break
+    try:
+        num1 = int(number)
+        if (num1 > largest):
+            largest = num1
+        if num1<smallest:
+            smallest = num1
+    except:
+        print('Invalid input')
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
